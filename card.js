0c4e7c9bfe9868290520c219ba616deddfc5c82a
yasbir-ovik <yasbir.634551@gmail.com>
2025-06-26 03:57:15 +0600

    file uploaded

---
+++ card.js
@@ -0,0 +1,14 @@
+const followBtn = document.getElementById('follow-btn');
+let isFollowing = false;
+followBtn.addEventListener('click', function () {
+    isFollowing = !isFollowing;
+    if (isFollowing) {
+        followBtn.textContent ="following";
+        followBtn.style.backgroundColor = "green";
+        followBtn.style.color = "white";
+    } else {
+        followBtn.textContent = "follow";
+        followBtn.style.backgroundColor = '#eee';
+        followBtn.style.color = "black";
+        }
+});
